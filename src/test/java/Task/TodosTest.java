811bb9e6ef11479bbefef9cdd7f070b411b757e6
Antonina Yurchenko <tonya2512@yandex.ru>
2023-10-15 16:34:35 +0300

    project init

--- src/test/java/Task/TodosTest.java
+++ src/test/java/Task/TodosTest.java
@@ -79,4 +79,28 @@ class TodosTest {
         Task[] actual = todos.search("Молоко");
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void whenZeroTaskFound() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+gi        Assertions.assertArrayEquals(expected, actual);
+    }
 }
